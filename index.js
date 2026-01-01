b500826e272b567b6ca69a60b0b8a06c32dc8230
vakariene1 <ggames201251@gmail.com>
2026-01-01 17:53:59 +0200

    clean

--- index.js
+++ index.js
@@ -6,21 +6,16 @@ game.height = 800
 
 const ctx = game.getContext("2d")
 
-
 function clear () {
     ctx.fillStyle = BACKGROUND
     ctx.fillRect(0, 0, game.width, game.height)
 }
-
-
 
 function point({x, y}) {
     const s = 20;
     ctx.fillStyle = FOREGROUND  
     ctx.fillRect(x - s/2, y - s/2, s, s)
 }
-
-
 
 function screen(p) {
     //-1..1 => 0..2 => 0...1 => 0..w
@@ -30,7 +25,7 @@ function screen(p) {
     }
 }
 
-function project({x, y, z}) {
+function project({x, y, z}) {  
     return {
         x: x/z,
         y: y/z,
@@ -39,13 +34,10 @@ function project({x, y, z}) {
 
 clear()
 
-
 point(screen(project({x: -0.5, y: 0.5, z: 2 })))
 point(screen(project({x: 0.5, y: -0.5, z: 1 +dz })))
 point(screen(project({x: -0.5, y: 0.5, z: 1 +dz })))
 
-
-//const { x, y } = screen({x: -0.5 , y: 0.5})
 
 
 const FPS = 60;
@@ -55,15 +47,4 @@ function frame() {
     const dt = 1/FPS
     dz += 1*dt
     clear()
-    
-    
-   
-}
-
-
-
-
-const box = {
-    x: 400,
-    y: 600
 }
