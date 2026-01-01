465e8a3a606cbccee91fe23675d7d5d9015d4dd0
vakariene1 <ggames201251@gmail.com>
2026-01-01 19:57:44 +0200

    add cube with strokes

--- index.js
+++ index.js
@@ -17,6 +17,14 @@ function point({x, y}) {
     ctx.fillRect(x - s/2, y - s/2, s, s)
 }
 
+function line(p1, p2) {
+    ctx.strokeStyle = FOREGROUND;
+
+    ctx.beginPath();
+    ctx.moveTo(p1.x, p1.y);
+    ctx.lineTo(p2.x, p2.y);
+    ctx.stroke();
+}
 function screen(p) {
     //-1..1 => 0..2 => 0...1 => 0..w
     return {
@@ -32,19 +40,65 @@ function project({x, y, z}) {
     }
 }
 
-clear()
-
-point(screen(project({x: -0.5, y: 0.5, z: 2 })))
-point(screen(project({x: 0.5, y: -0.5, z: 1 +dz })))
-point(screen(project({x: -0.5, y: 0.5, z: 1 +dz })))
-
-
-
 const FPS = 60;
-let dz = 0;
+let dz = 1;
+let angle = 0;
 
-function frame() {
-    const dt = 1/FPS
-    dz += 1*dt
-    clear()
+const vs = [
+    { x: -0.25, y:  0.25, z: 0.25},
+    { x:  0.25, y:  0.25, z: 0.25},
+    { x: -0.25, y: -0.25, z: 0.25},
+    { x:  0.25, y: -0.25, z: 0.25},
+    { x: -0.25, y:  0.25, z: -0.25 },
+    { x:  0.25, y:  0.25, z: -0.25 },
+    { x: -0.25, y: -0.25, z: -0.25 },
+    { x:  0.25, y: -0.25, z: -0.25 }
+]
+
+const fs = [
+    [0, 1, 2, 3],
+    [4, 5, 6, 7],
+]
+function translate_z({x, y, z}, dz) {
+    return {x, y, z: z + dz}
 }
+
+function rotate_xz ({x, y, z}, angle) {
+     const c = Math.cos(angle);
+     const s = Math.sin(angle);
+     return {
+        x: x*c-z*s,
+        y,
+        z: x*s+z*c,
+     }
+}
+function frame() {
+    const dt = 1/FPS;
+   // dz += 1*dt
+    angle += Math.PI*dt;
+    clear()
+    
+    for (const v of vs) {
+        point(screen(project(translate_z(rotate_xz(v, angle), dz))))
+    }
+
+    for (const f of fs) {
+        for (let i = 0; i < f.length; ++i) {
+
+            const a = vs[f[i]];
+            const b = vs[f[(i + 1)%f.length]];
+
+            const aStroke = screen(project(translate_z(rotate_xz(a, angle), dz)))
+            const bStroke = screen(project(translate_z(rotate_xz(b, angle), dz)))
+
+            line(
+                aStroke,
+                bStroke
+            )
+            
+        }       
+    }
+
+    setTimeout(frame, 1000/FPS); 
+}
+setTimeout(frame, 1000/FPS); 
